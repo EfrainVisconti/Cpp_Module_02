b297232c37a8fc03d77cfacb8de5659845f51c9f
Efrain Visconti <144735628+EfrainVisconti@users.noreply.github.com>
2024-10-28 23:59:35 +0000

    ex02 progress

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,7 +6,76 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:27:24 by codespace         #+#    #+#             */
-/*   Updated: 2024/10/28 22:27:25 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/28 23:57:15 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int  pow2(int fractionalBits)
+{
+    return 1 << fractionalBits; //Equivalent to 2 with exponent fractionalBits
+}
+
+Fixed::Fixed() : _value(0)
+{
+    //std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value)
+{
+    //std::cout << "Int constructor called" << std::endl;
+    this->_value = value << this->_fractionalBits; //Equivalent to multiply by 2 with exponent _fractionalBits
+}
+
+Fixed::Fixed(const float value)
+{
+    //std::cout << "Float constructor called" << std::endl;
+    this->_value = roundf(value * pow2(this->_fractionalBits));
+}
+
+Fixed::~Fixed()
+{
+    //std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    //std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed   &Fixed::operator=(const Fixed &other)
+{
+    //std::cout << "Copy assignment operator called" << std::endl;
+    if (this == &other)
+        return *this;
+    this->_value = other.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits() const
+{
+    return this->_value;
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
+
+float   Fixed::toFloat() const
+{
+    return ((float)this->_value / pow2(this->_fractionalBits));
+}
+
+int Fixed::toInt() const
+{
+    return this->_value >> _fractionalBits;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed &other)
+{
+    out << other.toFloat();
+    return out;
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,20 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:27:55 by codespace         #+#    #+#             */
-/*   Updated: 2024/10/28 22:27:56 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/28 22:51:42 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return 0;
+}
