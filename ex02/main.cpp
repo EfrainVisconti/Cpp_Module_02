feeff0122f1d0387097c2739886e4863d428bb51
Efrain Visconti <144735628+EfrainVisconti@users.noreply.github.com>
2024-10-30 20:40:50 +0000

    ex02/Fixed.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:27:55 by codespace         #+#    #+#             */
-/*   Updated: 2024/10/30 03:18:47 by codespace        ###   ########.fr       */
+/*   Updated: 2024/10/30 20:22:19 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ int main( void )
 {
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << a << std::endl;
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max( a, b ) << std::endl;
-
-    std::cout << Fixed::min( a, b ) << std::endl;
     
+    std::cout << b << std::endl;
+    
+    std::cout << Fixed::max( a, b ) << std::endl;
     return 0;
 }
